10a23dd1c5bf0e2bbbb43ebdd9cdc0d4df192d6c
SteveC39 <steve.corbett9@gmail.com>
2025-03-29 14:37:45 -0500

    Tests

--- binsearch.c
+++ binsearch.c
@@ -1,29 +1,40 @@
+// C Program to implement binary search using iteration
 #include <stdio.h>
 
-int binsearch(int yarr[10], int element)
-{
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
-    extern int i;
-    
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
-    {
-        for(i = 0; i < left; i++)
-        {
-            if(element == i)
-            {
-            //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        // Using bit shift for faster division by 2
+        int mid = left + ((right - left) >> 1);
+        
+        int midVal = arr[mid];  // Avoid multiple array accesses
+        
+        if (midVal == key) {
+            return mid;
+        }
+        
+        if (midVal < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-};
+    return -1;
+}
 
-int main(int argc, int argv[])
-{
-    binsearch(argv[0], argv[1]);
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Element to be searched
+    int key = 29;
+
+    int result = binarySearch(arr, 0, size - 1, key);
+    if (result == -1) {
+        printf("Element is not present in array");
+    }
+    else {
+        printf("Element is present at index %d", result);
+    }
+    return 0;
 }
